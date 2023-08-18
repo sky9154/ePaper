001c349d798f2a43922e7869726ce299387a4049
sky9154 <sky0932039612@gmail.com>
2023-08-18 11:53:11 +0800

    修復圖片錯誤

--- ePaper/Display.cpp
+++ ePaper/Display.cpp
@@ -1,17 +1,11 @@
 #include "Display.h"
 
 
-U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
-
 GxEPD2_7C<GxEPD2_730c_GDEY073D46, GxEPD2_730c_GDEY073D46::HEIGHT / 4> display(GxEPD2_730c_GDEY073D46(SS, 17, 16, 4));
 
 void Display::init(void) {
   display.init(115200, true, 2, false);
   display.setRotation(0);
-  u8g2Fonts.begin(display);
-  u8g2Fonts.setFont(msjh_44);
-  u8g2Fonts.setFontMode(1);
-  u8g2Fonts.setFontDirection(0);
 }
 
 
@@ -20,22 +14,8 @@ void Display::clear(void) {
 }
 
 
-void Display::text(String text, uint16_t font_color, int x, int y) {
-  display.setFullWindow();
-  display.firstPage();
-
-  u8g2Fonts.setForegroundColor(font_color);
-
-  do {
-    display.fillScreen(GxEPD_WHITE);
-    
-    u8g2Fonts.setCursor(x, y);
-    u8g2Fonts.println(text);
-  } while (display.nextPage());
-}
-
-
 void Display::draw(String image) {
+  display.setFullWindow();
   display.drawPaged(imageCallback, (const void*)image.c_str());
 }
 
@@ -52,40 +32,38 @@ void Display::getImage(String image) {
   HTTPClient http;
 
   String url = "http://192.168.0.38:5000/api/event/image/" + image;
-
   http.begin(url);
 
   int httpCode = http.GET();
 
   if (httpCode == HTTP_CODE_OK) {
     int len = http.getSize();
-    uint8_t buff[800] = { 0 };
+    uint8_t buff[256];
 
     WiFiClient *stream = http.getStreamPtr();
-
     int width = 0;
     int height = 0;
+
     while (http.connected() && (len > 0 || len == -1)) {
-      size_t size = stream -> available();
+      size_t size = stream->available();
 
       if (size) {
-        int c = stream -> readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+        int c = stream->readBytes(buff, min(sizeof(buff), size));
 
         for (int i = 0; i < c; i ++) {
-          if ((char)buff[i] != '"') {
+          char pixelData = buff[i];
 
-            display.drawPixel(width, height, color7((char)buff[i]));
+          if (pixelData != '"') {
+            uint16_t pixelColor = color7(pixelData);
+            display.drawPixel(width, height, pixelColor);
             width ++;
 
-            if (width > 800) {
+            if (width >= display.width()) {
               width = 0;
               height ++;
             }
           }
-        }
-
-        if (len > 0) {
-          len -= c;
+          len --;
         }
       }
     }
--- ePaper/Display.h
+++ ePaper/Display.h
@@ -1,24 +1,21 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include <U8g2_for_Adafruit_GFX.h>
 #include <GxEPD2_7C.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 
 
-extern U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
-
 class Display {
   public:
     void init();
     void clear();
-    void text(String text, uint16_t font_color, int x, int y);
-    static void draw(String image);
-    static uint16_t color7(char color);
+    void draw(String image);
+    
   private:
     static void imageCallback(const void* pv);
     static void getImage(String image);
+    static uint16_t color7(char color);
 };
 
 #endif
